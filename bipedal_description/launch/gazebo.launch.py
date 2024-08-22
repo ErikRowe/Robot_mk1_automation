b42a5de764775161b03bcd8e9f814dc07ca80b16
ErikRowe <ErikE.rowe@outlook.com>
2024-08-22 19:03:14 +0200

    renamed package

--- bipedal_description/launch/gazebo.launch.py
+++ bipedal_description/launch/gazebo.launch.py
@@ -10,9 +10,9 @@ from ament_index_python.packages import get_package_share_directory
 
 
 def generate_launch_description():
-    share_dir = get_package_share_directory('BiPedal_description')
+    share_dir = get_package_share_directory('bipedal_description')
 
-    xacro_file = os.path.join(share_dir, 'urdf', 'BiPedal.xacro')
+    xacro_file = os.path.join(share_dir, 'urdf', 'bipedal.xacro')
     robot_description_config = xacro.process_file(xacro_file)
     robot_urdf = robot_description_config.toxml()
 
@@ -58,7 +58,7 @@ def generate_launch_description():
         package='gazebo_ros',
         executable='spawn_entity.py',
         arguments=[
-            '-entity', 'BiPedal',
+            '-entity', 'bipedal',
             '-topic', 'robot_description'
         ],
         output='screen'
